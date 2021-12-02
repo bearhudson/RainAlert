76e84393bc3cc19008697516f078795df59e63e0
Brian Hudson <brian.hudson@flexential.com>
2021-12-02 11:40:34 -0500

    moved from address to environment variable.

--- email_message.py
+++ email_message.py
@@ -4,13 +4,14 @@ import os
 MY_EMAIL = os.environ.get('MY_EMAIL')
 MY_PASS = os.environ.get('MY_PASS')
 MY_SERVER = os.environ.get("MY_SERVER")
+MY_FROM = os.environ.get('MY_FROM')
 
 
 def email_message(message):
     with smtplib.SMTP_SSL(MY_SERVER) as connection:
         connection.login(user=MY_EMAIL, password=MY_PASS)
         connection.sendmail(
-            from_addr="python@flight-crew.org",
+            from_addr=MY_FROM,
             to_addrs=MY_EMAIL,
             msg=f"Subject: Precipitation Alert!"
                 f"\n\n{message}"
