6766fe45447881ecd44ec4777d3b966359f676e8
Brian Hudson <brian.hudson@flexential.com>
2021-12-02 19:43:21 -0500

    Move time interval to HOURS env variable.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # RainAlert
 
 ###### _Requires the following envvars:_
-* MY_SERVER
-* MY_EMAIL
-* MY_PASSWORD
-* MY_FROM
-* ZIPCODE
-* OPENWEATHER_API_KEY
+* MY_SERVER - _outgoing SMTP server_
+* MY_EMAIL - _your receiving email address_ 
+* MY_PASSWORD - _email password for login_ 
+* MY_FROM - _sending address_ 
+* ZIPCODE - _zipcode to monitor_ 
+* OPENWEATHER_API_KEY - _your openweather API key_ 
+* HOURS - _hours to monitor. note: higher numbers increase false positives_ 
--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 
 import requests
 import os
-import datetime
+import datetime as datetime
 import json
 import time
 from email_message import email_message
 
 API_KEY = os.environ.get("OPENWEATHER_API_KEY")
 ZIPCODE = os.environ.get("ZIPCODE")
-HOURS = 4
+HOURS = int(os.environ.get("HOURS"))
+SLEEP = 3600
 
 # load cond list
 with open("condition_codes.json", mode='r') as file:
@@ -35,9 +36,11 @@ while in_processing:
     request_api = requests.get(url=f'https://api.openweathermap.org/data/2.5/onecall', params=payload)
     request_api.raise_for_status()
     api_response_json = request_api.json()
-
+    now = time.time()
+    now_formatted = datetime.datetime.fromtimestamp(now).strftime('%H:%M--%y/%m/%d')
     cond_sum = 0
     cur_fc_txt = ""
+    print(f"\nT: {now_formatted}")
     weather_slice = api_response_json['hourly'][:HOURS]
     for hour_weather in weather_slice:
         cur_fc_time = hour_weather['dt']
@@ -50,7 +53,7 @@ while in_processing:
         print(f"{time_converted} o'clock forecast: {cur_fc_txt}")
         if cur_fc <= 622:
             email_message(f"Possible Precipitation soon!\n{time_converted} o'clock"
-                          f" forecast: {cur_fc_txt}")
+                          f" forecast: {cur_fc_txt}\nT: {now_formatted}")
 
     cur_cond = "Error"
     if cond_sum <= 232 * HOURS:
@@ -66,4 +69,4 @@ while in_processing:
     elif cond_sum <= 804 * HOURS:
         cur_cond = "None Forecasted"
     print(f"Overall Precipitation: {cur_cond}")
-    time.sleep(360)
+    time.sleep(SLEEP)
